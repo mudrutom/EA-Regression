f8a500feb0328e2bae310ffda615d634dae531a5
Tomas Mudrunka <mudrutom@gmail.com>
2013-05-07 00:18:46 +0200

    duplicate feature

--- src/main/java/cz/bia/ea/regression/model/impl/E.java
+++ src/main/java/cz/bia/ea/regression/model/impl/E.java
@@ -1,11 +1,18 @@
 package cz.bia.ea.regression.model.impl;
 
+import cz.bia.ea.regression.model.Expression;
+
 public class E extends Number {
 
 	public static final E E = new E();
 
 	public E() {
 		super(Math.E);
+	}
+
+	@Override
+	public Expression duplicate() {
+		return E;
 	}
 
 	@Override
--- src/main/java/cz/bia/ea/regression/model/impl/PI.java
+++ src/main/java/cz/bia/ea/regression/model/impl/PI.java
@@ -1,11 +1,18 @@
 package cz.bia.ea.regression.model.impl;
 
+import cz.bia.ea.regression.model.Expression;
+
 public class PI extends Number {
 
 	public static final PI PI = new PI();
 
 	public PI() {
 		super(Math.PI);
+	}
+
+	@Override
+	public Expression duplicate() {
+		return PI;
 	}
 
 	@Override
