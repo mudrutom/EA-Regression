99b16ffd2a66850ca4758fec6ac3f225ab0fcaa0
Tomas Mudrunka <mudrutom@gmail.com>
2013-05-10 00:01:01 +0200

    runtime updated

--- src/main/java/cz/bia/ea/regression/runtime/Runtime.java
+++ src/main/java/cz/bia/ea/regression/runtime/Runtime.java
@@ -7,12 +7,16 @@ import cz.bia.ea.regression.evolution.measure.ObjectiveFunction;
 import cz.bia.ea.regression.generate.DataGenerator;
 import cz.bia.ea.regression.generate.Function;
 import cz.bia.ea.regression.generate.Tuple;
-import cz.bia.ea.regression.model.Expression;
 import cz.bia.ea.regression.model.factory.ExpressionFactory;
 import cz.bia.ea.regression.model.impl.Number;
 import cz.bia.ea.regression.model.impl.Variable;
 import cz.bia.ea.regression.util.RandomNumbers;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static cz.bia.ea.regression.model.factory.CompositeExpression.*;
@@ -23,18 +27,18 @@ public class Runtime {
 		final Function f1 = new Function() {
 			@Override
 			public double eval(double x) {
-				return Math.cos(2 * x);
+				return Math.cos(2.0 * x);
 			}
 		};
-		final List<Tuple> data1 = DataGenerator.generateDataTuples(f1, 0.0, 2 * Math.PI, 200);
+		final List<Tuple> data1 = DataGenerator.generateDataTuples(f1, 0.0, 2.0 * Math.PI, 200);
 
 		final Function f2 = new Function() {
 			@Override
 			public double eval(double x) {
-				return Math.log(x);
+				return x*x*x - 2.0*x*x + x;
 			}
 		};
-		final List<Tuple> data2 = DataGenerator.generateDataTuples(f2, 0.1, 10, 200);
+		final List<Tuple> data2 = DataGenerator.generateDataTuples(f2, 0.0, 10.0, 100);
 
 		final RandomNumbers randomNumbers = new RandomNumbers();
 		final ExpressionFactory factory = new ExpressionFactory(randomNumbers);
@@ -45,30 +49,73 @@ public class Runtime {
 		final Evolution evolution = new Evolution(randomNumbers);
 		evolution.setExpressionFactory(factory);
 
-		runGP(data1, evolution);
-		runGA(data2, evolution);
+		runGP(data1, evolution, 10);
+		runGA(data2, evolution, 10);
 	}
 
-	private static void runGP(List<Tuple> data, Evolution evolution) {
+	private static void runGP(List<Tuple> data, Evolution evolution, int n) {
 		final GPConfiguration config = GPConfiguration.createDefaultConfig();
 		config.objective = ObjectiveFunction.MAE;
 		config.fitnessThreshold = 0.01;
 		config.initTreeDepth = 4;
 
-		final Expression expression1 = evolution.evolveTreeFor(data, config).result;
-		System.out.println(expression1.toStringExpression());
+		final List<Evolution.Result> results = new ArrayList<Evolution.Result>(n);
+		for (int i = 0; i < n; i++) {
+			results.add(evolution.evolveTreeFor(data, config));
+		}
+		exportResults("GP", results);
 	}
 
-	private static void runGA(List<Tuple> data, Evolution evolution) {
+	private static void runGA(List<Tuple> data, Evolution evolution, int n) {
 		final GAConfiguration config = GAConfiguration.createDefaultConfig();
 		config.objective = ObjectiveFunction.MAE;
-		config.fitnessThreshold = 0.01;
-		config.initPolyOrder = 4;
+		config.fitnessThreshold = 0.1;
+		config.initPolyOrder = 3;
 		config.paramRangeFrom = -5.0;
 		config.paramRangeTo = +5.0;
 
-		final Expression expression2 = evolution.evolvePolyFor(data, config).result;
-		System.out.println(expression2.toStringExpression());
+		final List<Evolution.Result> results = new ArrayList<Evolution.Result>(n);
+		for (int i = 0; i < n; i++) {
+			results.add(evolution.evolvePolyFor(data, config));
+		}
+		exportResults("GA", results);
+	}
+
+	private static void exportResults(String filePrefix, List<Evolution.Result> results) {
+		try {
+			final BufferedWriter out = new BufferedWriter(new FileWriter(filePrefix + ".out"));
+			final BufferedWriter fit = new BufferedWriter(new FileWriter(filePrefix + ".fit"));
+			final BufferedWriter time = new BufferedWriter(new FileWriter(filePrefix + ".time"));
+
+			for (Evolution.Result result : results) {
+				out.write(Double.toString(result.fitness));
+				out.write(' ');
+				out.write(Long.toString(result.time));
+				out.write(' ');
+				out.write(Integer.toString(result.epochs));
+				out.write('\n');
+				out.flush();
+
+				Iterator<Double> fitIter = result.fitnessProgress.iterator();
+				Iterator<Long> timeIter = result.timeProgress.iterator();
+				while (fitIter.hasNext()) {
+					fit.write(Double.toString(fitIter.next()));
+					fit.write(' ');
+					time.write(Long.toString(timeIter.next()));
+					time.write(' ');
+				}
+				fit.write('\n');
+				fit.flush();
+				time.write('\n');
+				time.flush();
+			}
+
+			out.close();
+			fit.close();
+			time.close();
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
 	}
 
 }
